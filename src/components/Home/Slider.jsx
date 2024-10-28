14207cc6e83c66a98a26a93bbf4df9996420da6d
mo7amed4a <m7md0a99@gmail.com>
2024-10-28 15:35:40 +0300

    slider

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -39,12 +39,12 @@ export default function SliderApp({
                 {/* <Image src={slide.url === "/images/img27.jpg" ? slide.url : baseURL + slide.url} className='w-full' alt="Image" width={1000} height={1000} /> */}
                 <div className="absolute inset-0 flex flex-col justify-center items-center text-center p-4">
               <div className="flex flex-col items-center w-full space-y-5">
-                <h1 className="text-white text-5xl font-bold mb-2">
+                <h1 className="text-white text-2xl md:text-5xl font-bold mb-2">
                   <span className="block">
                     {slide.title}
                   </span>
                 </h1>
-                <p className="text-white text-2xl mb-9 w-5/6 md:w-2/3">{slide.text}</p>
+                <p className="text-white md:text-2xl mb-9 w-5/6 md:w-2/3">{slide.text}</p>
                 <button className="bg-sky-700 text-white px-8 py-3 hover:bg-sky-900">
                   CONTINUE READING
                 </button>
